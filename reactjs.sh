f41a5b7c54399fa1bad48d12779e369e4e781612
DevMagical <elias.chaabane@gmail.com>
2024-06-21 15:09:56 +0200

    Update reactjs.sh

--- reactjs.sh
+++ reactjs.sh
@@ -1,8 +1,16 @@
-# Créez une nouvelle application React
-npx create-react-app resto-proch
+#!/bin/bash
 
-# Naviguez vers le répertoire du projet
-cd resto-proch
+# Variables
+KEY_PATH="~/.ssh/MyKeyPair.pem"
+FRONT_END_IPS=("front-end-vm-ip-1" "front-end-vm-ip-2")
 
-# Construisez l'application React
+# Build the React application
 npm run build
+
+# Test the React application
+npm test
+
+# Deploy the build to the front-end VMs
+for ip in "${FRONT_END_IPS[@]}"; do
+    scp -i $KEY_PATH -r build/* ec2-user@$ip:/var/www/html
+done
