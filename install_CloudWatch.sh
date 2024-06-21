c2e326019511122b224375576e2edd13d1a48842
DevMagical <elias.chaabane@gmail.com>
2024-06-21 15:10:43 +0200

    Create install_CloudWatch.sh

---
+++ install_CloudWatch.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Variables
+FRONT_END_IPS=("front-end-vm-ip-1" "front-end-vm-ip-2")
+BACK_END_IPS=("back-end-vm-ip-1" "back-end-vm-ip-2")
+KEY_PATH="~/.ssh/MyKeyPair.pem"
+
+# Command to install CloudWatch agent
+INSTALL_CW_AGENT="sudo yum install -y amazon-cloudwatch-agent"
+
+# Command to configure CloudWatch agent
+CONFIGURE_CW_AGENT="sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-config-wizard"
+
+# Command to start CloudWatch agent
+START_CW_AGENT="sudo systemctl start amazon-cloudwatch-agent"
+
+# Loop through all instances and install, configure, and start CloudWatch agent
+for ip in "${FRONT_END_IPS[@]}" "${BACK_END_IPS[@]}"; do
+    ssh -i $KEY_PATH ec2-user@$ip $INSTALL_CW_AGENT
+    ssh -i $KEY_PATH ec2-user@$ip $CONFIGURE_CW_AGENT
+    ssh -i $KEY_PATH ec2-user@$ip $START_CW_AGENT
+done
