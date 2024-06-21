2768f48d44987a6855d1af1fe23d7b90d2213d4d
DevMagical <elias.chaabane@gmail.com>
2024-06-21 15:09:12 +0200

    Create reactjs.sh

---
+++ reactjs.sh
@@ -0,0 +1,8 @@
+# Créez une nouvelle application React
+npx create-react-app resto-proch
+
+# Naviguez vers le répertoire du projet
+cd resto-proch
+
+# Construisez l'application React
+npm run build
